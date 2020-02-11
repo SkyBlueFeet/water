807da162f372481c2d0599a3896679f1b2f15c67
SkyBlue <yakun1593@163.com>
2020-02-11 19:01:03 +0800

    refactor(add vuessr): 实现服务端渲染
    删去ts配置部分，实现ssr

--- script/dev.ts
+++ script/dev.ts
@@ -1,15 +1,3 @@
-import WebpackDevServer from "webpack-dev-server";
-import webpack, { Configuration } from "webpack";
-
 process.env.NODE_ENV = "development";
 
-import setConfig from "../config";
-
-setConfig("development")
-    .then((devConfig: Configuration) => {
-        const Compile = webpack(devConfig);
-        const app = new WebpackDevServer(Compile, devConfig.devServer);
-
-        app.listen(devConfig.devServer.port, error => console.error(error));
-    })
-    .catch(reason => console.error(reason));
+import "../example";
